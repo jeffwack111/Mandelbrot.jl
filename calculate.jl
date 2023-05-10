5b6e053cf5ecfa5da1b3e693c0f8671577e8e1d5
Jeffrey Wack <jeffwack111@gmail.com>
2023-05-10 00:17:54 -0400

    changes

---
+++ calculate.jl
@@ -0,0 +1,16 @@
+include("Spiders.jl")
+
+K = KneadingSequence(333//1604)
+frames = 20
+
+list = [SpiderLegs(K.orbit)]
+
+
+for i in 1:frames-1
+    SL = list[end]
+    push!(list,spider_map(SL,K))
+end
+
+
+
+
