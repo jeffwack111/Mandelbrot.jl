dc0150e49034a34572c1ed65ee7f05981c57c69f
Jeffrey Wack <jeffwack111@gmail.com>
2023-06-30 16:26:53 -0400

    added preperiodic sequence

--- Sequences.jl
+++ Sequences.jl
@@ -6,10 +6,34 @@ function Base.getindex(P::PeriodicSequence,i::Int)
     return P.S[mod1(i,end)]
 end
 
+function Base.getindex(P::PeriodicSequence, I) 
+    return [P[i] for i in I]
+end
+
 function Base.size(P::PeriodicSequence)
     return Base.size(P.S)
 end
 
-function Base.getindex(P::PeriodicSequence, I) 
+struct PrePeriodicSequence{T} <: AbstractVector{T}
+    L::Vector{T}
+    K::Vector{T}
+end
+
+function Base.size(P::PrePeriodicSequence)
+    return Base.size(P.L).+Base.size(P.K)
+end
+
+function Base.getindex(P::PrePeriodicSequence,i::Int)
+    l = length(P.L)
+    if i<=0
+        throw(DomainError(i, "index must be positive"))
+    elseif i <= l
+        return P.L[i]
+    else
+        return P.K[mod1(i - l,end)]
+    end
+end
+
+function Base.getindex(P::PrePeriodicSequence, I) 
     return [P[i] for i in I]
 end
