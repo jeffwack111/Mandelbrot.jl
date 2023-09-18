b5becccfa9c6465badd4fda7d661790e703dd74f
Jeffrey Wack <jeffwack111@gmail.com>
2023-09-17 23:21:07 -0400

    now uses Sequence type

--- AngleDoubling.jl
+++ AngleDoubling.jl
@@ -42,14 +42,15 @@ function binary(angle::Rational)
 
 end
 
-function internal_address(periodicsequence::Vector,mapsto::Vector{Int})
-    if mapsto[end] == 1
+#works only for strictly periodic kneading sequences
+function internaladdress(K::Sequence)
+    if K.preperiod == 0
         S = [1]
         v = ['A']
-        for ii in eachindex(periodicsequence)
-            if periodicsequence[ii] != v[mod1(ii,end)]
+        for ii in eachindex(K.items)
+            if K.items[ii] != v[mod1(ii,end)]
                 push!(S,ii)
-                v = periodicsequence[1:ii]
+                v = K.items[mod1.(1:ii,end)]
             end
         end
         return S
@@ -58,22 +59,23 @@ function internal_address(periodicsequence::Vector,mapsto::Vector{Int})
     end
 end
 
-function internal_address(angle::Rational)
-    return internal_address(kneading_sequence(angle)...)    
+function internaladdress(angle::Rational)
+    return internaladdress(kneadingsequence(angle))    
 end
 
-function kneading_sequence(internaladdress::Vector{Int})
-    if internal_address == [1]
-        return PeriodicSequence([1])
+#Works only for finite internal addresses
+function kneadingsequence(internaladdress::Vector{Int})
+    if internaladdress == [1]
+        return Sequence(['A'],0)
     else
-        s = pop!(internal_address)
-        K = kneading_sequence(internal_address)
-        R = K[1:s-1]
-        if K[s] == 0
-            push!(R,1)
+        s = pop!(internaladdress)
+        K = kneadingsequence(internaladdress)
+        R = K.items[mod1.(1:s-1,end)]
+        if K.items[mod1(s,end)] == 'A'
+            push!(R,'B')
         else
-            push!(R,0)
+            push!(R,'A')
         end
-        return PeriodicSequence(R)
+        return Sequence(R,0)
     end
 end
