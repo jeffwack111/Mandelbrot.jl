3d82ae14bbecd665ba9bcaf95148bedc61d14a7c
Jeffrey Wack <jeffwack111@gmail.com>
2023-12-05 21:49:40 -0800

    gleason polynomials

--- misc/RootFinding.jl
+++ misc/RootFinding.jl
@@ -1,4 +1,9 @@
 using Symbolics
+using Primes
+
+#This seems to tap out at order 8, where it returns a polynomial with teeny tiny coefficients
+
+@variables z,c
 
 function composite(order::Int)
     if order==1
@@ -8,7 +13,32 @@ function composite(order::Int)
     end
 end
 
+function divisors(n)
+    d = Int64[1]
+    for (p, e) in factor(n)
+        r = 1
+        l = length(d)
+        for i in 1:e
+            r *= p
+            for j in 1:l
+                push!(d, d[j]*r)
+            end
+        end
+    end
+    return sort(d)
+end
+
 function gleason(order::Int)
-    return expand(substitute(composite(order),z=>0))
+    if order==1
+        return c
+    end
+    f = substitute(composite(order),z=>0)
+    D = divisors(order)
+    pop!(D)
+    for d in D
+        f = simplify(f/gleason(d))
+    end
+    return f
+
 end
 
