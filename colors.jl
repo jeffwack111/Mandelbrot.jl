4ef5da0c6700a795362b58db98591d4baa1b25bf
Jeffrey Wack <jeffwack111@gmail.com>
2024-08-17 09:52:28 -0400

    more colors

--- colors.jl
+++ colors.jl
@@ -2,19 +2,32 @@ using ColorSchemes
 using Colors
 using CairoMakie
 
-n = 4
-colors = [Colors.HSL{Float64}(360*i/n, 1, 0.5) for i in 0:(n-1)]
-print(colors)
-data = ones(n)
+n = 18
+colors = [Colors.HSL{Float64}(30+360*i/n, 1, 0.5) for i in 0:(n-1)]
+scene = Scene(size = (500,500))
 
-f, ax, plt = pie(data,
+function treecolors(A,B,distance)
+    sat = 0.8
+    light = 0.5
+    return [Colors.HSL{Float64}(A, sat, light),
+            Colors.HSL{Float64}(A+distance/4, sat, light),
+            Colors.HSL{Float64}(A+distance/2, sat, light),
+            Colors.HSL{Float64}(B-distance, sat, light),
+            Colors.HSL{Float64}(B-distance/2, sat, light),
+            Colors.HSL{Float64}(B, sat, light)]
+end
+
+colors = treecolors(0,220,70)
+
+pie!(scene,ones(length(colors)),
                  color = colors,
-                 radius = 4,
-                 inner_radius = 2,
+                 radius = 1,
+                 inner_radius = 0.1,
                  strokecolor = :white,
-                 strokewidth = 5,
-                 axis = (autolimitaspect = 1, )
+                 strokewidth = 0,
                 )
 
-f
+                
+y = Colors.RGB(1,1,0)
 
+scene
